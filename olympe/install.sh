e3d5cc92e7248033a1cac81af9edcb20b6287a23
Martin Falang <falang.martin@gmail.com>
2021-09-15 14:35:47 +0200

    Add scripts to install and start olympe

---
+++ olympe/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}========== Installing Parrot Olympe ==========${NC}"
+
+echo -e "${GREEN}\nInstalling Repo tool${NC}"
+mkdir -p ~/.bin
+PATH="${HOME}/.bin:${PATH}"
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
+chmod a+rx ~/.bin/repo
+
+echo -e "${GREEN}\nCloning source code${NC}"
+cd /home/$USER
+mkdir -p code/parrot-groundsdk
+cd code/parrot-groundsdk
+repo init -u https://github.com/Parrot-Developers/groundsdk-manifest.git
+repo sync
+
+echo -e "${GREEN}\nInstalling dependencies${NC}"
+./products/olympe/linux/env/postinst
+
+echo -e "${GREEN}\nBuilding Olympe${NC}"
+./build.sh -p olympe-linux -A all final -j
+
+echo -e "${GREEN}\nFixing Python path in Olympe navtive wrapper script${NC}"
+sed -i '/# Update python path/!b;n;c\PYTHONPATH=${PYTHONPATH}:${SYSROOT}/usr/lib/python/site-packages' ./out/olympe-linux/final/native-wrapper.sh
+
+echo -e "${GREEN}\nInstalling Olympe requirements in current Python environment${NC}"
+pip3 install -r "./packages/olympe/requirements.txt"
+
+echo -e "${GREEN}\nRemoving Repo tool${NC}"
+rm -rf ~/.bin
+
+echo -e "${GREEN}========== Installation finished ==========${NC}"
---
+++ olympe/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}========== Starting Parrot Olympe environment ==========${NC}"
+source "$HOME/code/parrot-groundsdk/out/olympe-linux/final/native-wrapper.sh"
