3282205d1c8526977140fab2a9a8c360bd157b84
Martin Falang <falang.martin@gmail.com>
2021-09-28 13:16:39 +0200

    Clean up ground truth script a bit

--- src/catkin_ws/src/melodic_sandbox/src/ground_truth.py
+++ src/catkin_ws/src/melodic_sandbox/src/ground_truth.py
@@ -1,73 +1,14 @@
 #!/usr/bin/env python3
 
-from os import times
 from subprocess import PIPE, Popen
-from threading  import Thread
 import sys
-import numpy as np
-import re
-from queue import Queue, Empty
 
-# Process the output from the file
-def process_output(out, queue):
-    
-    next_pos = 0
-    
-    for line in iter(out.readline, b''):
-        line = line.decode("utf-8")
-        print(line)
-        if next_pos and next_pos <= 3:
-            number = re.findall(r"[-+]?\d*\.\d+|\d+", line)[0]
-            if "x" in line:
-                pos1[0] = float(number)
-            if "y" in line:
-                pos1[1] = float(number)
-            if "z" in line:
-                pos1[2] = float(number)
-            next_pos += 1
-        elif "position" in line:
-            next_pos = 1
-        else:    
-            next_pos = 0
-    
-        # if ".worldPosition" in line:
-        #     number = re.findall(r"[-+]?\d*\.\d+|\d+", line)[1]
-        #     if ".x" in line:
-        #         pos1[0] = float(number)
-        #     if ".y" in line:
-        #         pos1[1] = float(number)
-        #     if ".z" in line:
-        #         pos1[2] = float(number)
-
-
-        # if ".worldAttitude" in line:
-        #     number = re.findall(r"[-+]?\d*\.\d+|\d+", line)[1]
-        #     if ".x" in line:
-        #         att1[0] = float(number)
-        #     if ".y" in line:
-        #         att1[1] = float(number)
-        #     if ".z" in line:
-        #         att1[2] = float(number)
-                
-        queue.put(line)
-    out.close()
+def publish_data(data):
+    print(data)
+    print()
+    print()
 
 if __name__ == "__main__":
-    pos1 = np.zeros(3)
-    att1 = np.zeros(3)
-    q = Queue()
-
-    # Run the command
-    ON_POSIX = 'posix' in sys.builtin_module_names
-    command = "parrot-gz topic -e /gazebo/land/pose/info | grep -E -A 12 " \
-        "'time" \
-        "|name: \"helipad\"" \
-        "|name: \"anafi4k\"'" 
-    p = Popen(command, stdout=PIPE, bufsize=1, close_fds=ON_POSIX, shell=True)
-
-    
-    msg = {}
-    next_pos = 0
 
     entry = {
         "timestamp": {
@@ -101,6 +42,13 @@ if __name__ == "__main__":
             },
         }
     }
+
+    ON_POSIX = 'posix' in sys.builtin_module_names
+    command = "parrot-gz topic -e /gazebo/land/pose/info | grep -E -A 12 " \
+        "'time" \
+        "|name: \"helipad\"" \
+        "|name: \"anafi4k\"'" 
+    p = Popen(command, stdout=PIPE, bufsize=1, close_fds=ON_POSIX, shell=True)
     
     timestamp_data_gather_done = False
     anafi_data_gather_done = False
@@ -183,37 +131,11 @@ if __name__ == "__main__":
             helipad_index = 1
         
         if timestamp_data_gather_done and anafi_data_gather_done and helipad_data_gather_done:
-            print(entry)
-            break
-
-        # # Sample that works on only anafi data
-        # if next_pos and next_pos <= 3:
-        #     number = re.findall(r"[-+]?\d*\.\d+|\d+", line)[0]
-        #     msg[line[line.find(":")-1]] = float(number)
-        #     next_pos += 1
-        #     if next_pos == 4:
-        #         print(msg)
-        #         msg = {}
-        #         next_pos = 0
-        # elif "position" in line:
-        #     next_pos = 1
+            publish_data(entry)
+            timestamp_data_gather_done = False
+            anafi_data_gather_done = False
+            helipad_data_gather_done = False
     
-    sys.exit()
+    
 
-    # Create a thread which dies with main program
-    t = Thread(target=process_output, args=(p.stdout, q))
-    t.daemon = True 
-    t.start()
-
-    for i in range(100):
-        try:  
-            line = q.get_nowait()
-        except Empty:
-            # Clear out the queue
-            q.queue.clear()
-
-        print("Position: {}, {}, {}".format(pos1[0], pos1[1], pos1[2]))
-        print("Orientation: {}, {}, {}".format(att1[0], att1[1], att1[2]))
-
-    print("System Exiting\n")
-    sys.exit(0)
+    
