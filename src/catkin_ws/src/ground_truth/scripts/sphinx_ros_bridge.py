1be24732953f1a698393100978dbcff8d3aea9d1
Martin Falang <falang.martin@gmail.com>
2021-11-10 09:19:36 +0100

    Make helipad pose publish last known pose when not in contact with Anafi and not moving

--- src/catkin_ws/src/ground_truth/scripts/sphinx_ros_bridge.py
+++ src/catkin_ws/src/ground_truth/scripts/sphinx_ros_bridge.py
@@ -8,19 +8,23 @@ import geometry_msgs.msg
 class SphinxRosBridge():
 
     def __init__(self):
+        """
+        Bridge between Sphinx simulator and ROS, which publishes the ground
+        truth pose of the drone and the helipad .
+        """
         rospy.init_node("sphinx_ros_bridge", anonymous=False)
         self.anafi_pose_publisher = rospy.Publisher(
-            "sphinx/anafi_pose", geometry_msgs.msg.PoseStamped, queue_size=10
+            "ground_truth/pose/drone", geometry_msgs.msg.PoseStamped, queue_size=10
         )
-        # self.helipad_pose_publisher = rospy.Publisher(
-        #     "sphinx/helipad_pose", geometry_msgs.msg.PoseStamped, queue_size=10
-        # )
+        self.helipad_pose_publisher = rospy.Publisher(
+            "ground_truth/pose/helipad", geometry_msgs.msg.PoseStamped, queue_size=10
+        )
 
         self.data = {
         "timestamp": {
             "sec": None,
             "nsec": None
-        }, 
+        },
         "anafi": {
             "position": {
                 "x": None,
@@ -50,92 +54,122 @@ class SphinxRosBridge():
     }
 
     def start(self):
-        
+        """
+        This function will block and run infinitely until either the ROS-node is
+        killed or the Sphinx simulator is killed.
+        """
         rospy.loginfo("Starting Sphinx-Ros-Bridge")
 
         while not rospy.is_shutdown():
-            
+
             ON_POSIX = 'posix' in sys.builtin_module_names
             command = "parrot-gz topic -e /gazebo/land/pose/info | grep -E -A 12 " \
                 "'time" \
                 "|name: \"helipad\"" \
-                "|name: \"anafi4k\"'" 
-            p = Popen(command, stdout=PIPE, bufsize=1, close_fds=ON_POSIX, shell=True)
-            
-            timestamp_data_gather_done = False
-            anafi_data_gather_done = False
-            helipad_data_gather_done = False
+                "|name: \"anafi4k\"'"
 
-            timestamp_index = 0
-            anafi_index = 0
-            helipad_index = 0
-            
-            # Parse output
-            for line in iter(p.stdout.readline, b''):
-                line = line.decode("utf-8")
+            with Popen(command, stdout=PIPE, bufsize=1, close_fds=ON_POSIX, shell=True) as p:
 
-                # First find timestamp as this will be the first value
-                if timestamp_index > 0 and timestamp_index <= 3:
-                    # Gather data
-                    if timestamp_index < 3:
-                        line = line.split()
-                        self.data["timestamp"][line[0][:-1]] = int(line[1])
-                        timestamp_index += 1
-                    # Timestamp index is 3 and there is no more data to gather
-                    else:
-                        timestamp_index = 0
-                        timestamp_data_gather_done = True
+                timestamp_data_gather_done = False
 
-                elif "time" in line:
-                    timestamp_index = 1
+                timestamp_index = 0
+                anafi_index = 0
+                helipad_index = 0
 
-                # Make sure to only search for data after a timestamp is found to make
-                # sure that the data belong to the correct timestamp
-                if timestamp_data_gather_done == False:
-                    continue
+                # Parse output
+                for line in iter(p.stdout.readline, b''):
+                    line = line.decode("utf-8")
 
-                # Find Anafi data
-                if anafi_index > 0 and anafi_index <= 12:
-                    # Skip over two first entries before position data
-                    if anafi_index < 3:
-                        anafi_index += 1
-                    # Parse anafi position
-                    elif anafi_index >= 3 and anafi_index <= 5:
-                        line = line.split()
-                        self.data["anafi"]["position"][line[0][0]] = float(line[1])
-                        anafi_index += 1
-                    # Skip over two first entries before orientation data
-                    elif anafi_index < 8:
-                        anafi_index += 1
-                    # Parse anafi orientation
-                    elif anafi_index >= 8 and anafi_index <= 11:
-                        line = line.split()
-                        self.data["anafi"]["orientation"][line[0][0]] = float(line[1])
-                        anafi_index += 1
-                    # Anafi index is 12 and there is no more data to gather
-                    else:
-                        anafi_index = 0
-                        anafi_data_gather_done = True
-                elif "anafi" in line:
-                    anafi_index = 1
+                    # First find timestamp as this will be the first value
+                    if timestamp_index > 0 and timestamp_index <= 3:
+                        # Gather data
+                        if timestamp_index < 3:
+                            line = line.split()
+                            self.data["timestamp"][line[0][:-1]] = int(line[1])
+                            timestamp_index += 1
+                        # Timestamp index is 3 and there is no more data to gather
+                        else:
+                            timestamp_index = 0
+                            timestamp_data_gather_done = True
 
-                if timestamp_data_gather_done and anafi_data_gather_done:
-                    anafi_pose = self._pack_message("anafi")
-                    self.anafi_pose_publisher.publish(anafi_pose)
-                    timestamp_data_gather_done = False
-                    anafi_data_gather_done = False
-                    # TODO: Find out if there is a faster way to publish the 
-                    # helipad pose as this method is too slow. Unfortunately, 
-                    # the helipad pose is not available in the pose topic when
-                    # the helipad is not in contact with the drone, so that is
-                    # why this workaround was tried.
-                    # self._get_helipad_pose()
-                    # helipad_pose = self._pack_message("helipad")
-                    # self.helipad_pose_publisher.publish(helipad_pose)
+                    # Found start of a new entry -> pubish poses from previous entry
+                    elif "time" in line:
+                        self._publish_poses()
+                        timestamp_data_gather_done = False
+                        timestamp_index = 1
 
-    def _get_helipad_pose(self):
+                    # Make sure to only search for data after a timestamp is found to make
+                    # sure that the data belong to the correct timestamp
+                    if timestamp_data_gather_done == False:
+                        continue
+
+                    # Find Anafi data
+                    if anafi_index > 0 and anafi_index <= 12:
+                        # Skip over two first entries before position data
+                        if anafi_index < 3:
+                            anafi_index += 1
+                        # Parse anafi position
+                        elif anafi_index >= 3 and anafi_index <= 5:
+                            line = line.split()
+                            self.data["anafi"]["position"][line[0][0]] = float(line[1])
+                            anafi_index += 1
+                        # Skip over two first entries before orientation data
+                        elif anafi_index < 8:
+                            anafi_index += 1
+                        # Parse anafi orientation
+                        elif anafi_index >= 8 and anafi_index <= 11:
+                            line = line.split()
+                            self.data["anafi"]["orientation"][line[0][0]] = float(line[1])
+                            anafi_index += 1
+                        # Anafi index is 12 and there is no more data to gather
+                        else:
+                            anafi_index = 0
+                    elif "anafi" in line:
+                        anafi_index = 1
+
+                    # Find helipad data
+                    if helipad_index > 0 and helipad_index <= 12:
+                        # Skip over two first entries before position data
+                        if helipad_index < 3:
+                            helipad_index += 1
+                        # Parse helipad position
+                        elif helipad_index >= 3 and helipad_index <= 5:
+                            line = line.split()
+                            self.data["helipad"]["position"][line[0][0]] = float(line[1])
+                            helipad_index += 1
+                        # Skip over two first entries before orientation data
+                        elif helipad_index < 8:
+                            helipad_index += 1
+                        # Parse helipad orientation
+                        elif helipad_index >= 8 and helipad_index <= 11:
+                            line = line.split()
+                            self.data["helipad"]["orientation"][line[0][0]] = float(line[1])
+                            helipad_index += 1
+                        # helipad index is 12 and there is no more data to gather
+                        else:
+                            helipad_index = 0
+                    elif "helipad" in line:
+                        helipad_index = 1
+
+    def _publish_poses(self):
+        anafi_pose = self._pack_message("anafi")
+        if anafi_pose is not None:
+            self.anafi_pose_publisher.publish(anafi_pose)
+
+        helipad_pose = self._pack_message("helipad")
+        if helipad_pose is not None:
+            self.helipad_pose_publisher.publish(helipad_pose)
+
+    def _get_helipad_pose_from_model_info(self):
+        """
+        Function to get helipad pose from the model information from Gazebo.
+        This was first created as a workaround since the Gazebo-topic stopped
+        publishing after the Anafi did not make contact with the landing pad
+        anymore, but this method was much slower. Left in in case it will be
+        needed for something in the future.
+        """
         command = "parrot-gz model -m helipad -i | head -14 | tail -9"
-        
+
         p = Popen(command, stdout=PIPE, shell=True)
 
         self.data["helipad"]["position"]["x"] = float(p.stdout.readline().decode("utf-8").split()[1])
@@ -147,8 +181,13 @@ class SphinxRosBridge():
         self.data["helipad"]["orientation"]["y"] = float(p.stdout.readline().decode("utf-8").split()[1])
         self.data["helipad"]["orientation"]["z"] = float(p.stdout.readline().decode("utf-8").split()[1])
         self.data["helipad"]["orientation"]["w"] = float(p.stdout.readline().decode("utf-8").split()[1])
-        
+
     def _pack_message(self, model):
+
+        # Return None if data has not been initialized yet
+        if None in self.data["timestamp"].values():
+            return None
+
         model_pose = geometry_msgs.msg.PoseStamped()
         model_pose.header.stamp.secs = self.data["timestamp"]["sec"]
         model_pose.header.stamp.nsecs = self.data["timestamp"]["nsec"]
