32fa939d0cc6498b7edfe5197c74b954d34f34c3
Martin Falang <falang.martin@gmail.com>
2021-11-21 22:18:16 +0100

    Add EKF output plot, change arguments

--- src/catkin_ws/src/utilities/scripts/visualize.py
+++ src/catkin_ws/src/utilities/scripts/visualize.py
@@ -13,67 +13,78 @@ class Plotter():
         script_dir = os.path.dirname(os.path.realpath(__file__))
         self.data_dir = f"{script_dir}/../../../../../out/{data_dir}"
 
-        self.figure = plt.figure()
-        self.ax = plt.axes(projection='3d')
-        self.ax.set_xlabel("x [m]")
-        self.ax.set_ylabel("y [m]")
-        self.ax.set_zlabel("z [m]")
-
+        self.pose3D_figure = plt.figure()
+        self.pose3D_ax = plt.axes(projection='3d')
+        self.pose3D_ax.set_xlabel("x [m]")
+        self.pose3D_ax.set_ylabel("y [m]")
+        self.pose3D_ax.set_zlabel("z [m]")
 
     def plot_drone_ground_truth(self):
         print("Plotting drone ground truth")
         pose = np.loadtxt(f"{self.data_dir}/ground_truths/drone_pose.txt", skiprows=1)
-        self.ax.plot3D(pose[:,1], pose[:,2], pose[:,3], label="Drone ground truth")
+        self.pose3D_ax.plot3D(pose[:,1], pose[:,2], pose[:,3], label="Drone ground truth")
         plt.legend()
 
     def plot_helipad_ground_truth(self):
         print("Plotting helipad ground truth")
         pose = np.loadtxt(f"{self.data_dir}/ground_truths/helipad_pose.txt", skiprows=1)
-        self.ax.plot3D(pose[:,1], pose[:,2], pose[:,3], label="Helipad ground truth")
+        self.pose3D_ax.plot3D(pose[:,1], pose[:,2], pose[:,3], label="Helipad ground truth")
         plt.legend()
 
-    def plot_drone_estimate(self):
+    def plot_drone_pose_dnn_cv(self):
         print("Plotting drone estimate raw from DNN CV")
         pose = np.loadtxt(f"{self.data_dir}/estimates/dnn_cv_pose.txt", skiprows=1)
-        self.ax.plot3D(pose[:,1], pose[:,2], pose[:,3], label="Drone DNN CV estimate")
+        self.pose3D_ax.plot3D(pose[:,1], pose[:,2], pose[:,3], label="Drone DNN CV estimate")
         plt.legend()
+
+    def plot_drone_pose_ekf(self):
+        print("Plotting drone estimate from EKF and covariance")
+        output = np.loadtxt(f"{self.data_dir}/estimates/ekf_output.txt", skiprows=1)
+        self.pose3D_ax.plot3D(output[:,1], output[:,2], output[:,3], label="Drone EKF estimate")
+        plt.legend()
+
+    def plot_drone_pose_cov(self):
+        print("Plotting drone pose estimate covariance")
+        output = np.loadtxt(f"{self.data_dir}/estimates/ekf_output.txt", skiprows=1)
+
+    
 
     def plot_helipad_estimate(self):
         print("Plotting helipad estimate (not implemented)")
         # pose = np.loadtxt(f"{self.data_dir}/estimate/helipad/pose.txt", skiprows=1)
-        # self.ax.plot3D(pose[:,1], pose[:,2], pose[:,3], label="Helipad estimate")
+        # self.pose3D_ax.plot3D(pose[:,1], pose[:,2], pose[:,3], label="Helipad estimate")
         # plt.legend()
 
 
 def main():
     parser = argparse.ArgumentParser(description="Visualize data.")
     parser.add_argument("data_dir", metavar="data_dir", type=str, help="Base directory of data")
-    parser.add_argument("--drone_plots", choices=["all", "est", "gt", "none"],
-        help="What drone data to visualize (default: all)", default="all"
+    parser.add_argument("--estimate_plots", choices=["all", "ekf_pose", "dnn_cv_pose", "none"],
+        help="What estimation data to visualize (default: all)", default="all"
     )
-    parser.add_argument("--helipad_plots", choices=["all", "est", "gt", "none"],
-        help="What helipad data to visualize (default: all)", default="all"
+    parser.add_argument("--gt_plots", choices=["all", "drone_pose", "helipad_pose", "none"],
+        help="What ground truth data to visualize (default: all)", default="all"
     )
 
     args = parser.parse_args()
-
+    
     plotter = Plotter(args.data_dir)
 
-    if args.drone_plots == "all":
+    if args.gt_plots == "all":
         plotter.plot_drone_ground_truth()
-        plotter.plot_drone_estimate()
-    elif args.drone_plots == "est":
-        plotter.plot_drone_estimate()
-    elif args.drone_plots == "gt":
+        plotter.plot_helipad_ground_truth()
+    elif args.gt_plots == "drone_pose":
         plotter.plot_drone_ground_truth()
+    elif args.gt_plots == "helipad_pose":
+        plotter.plot_helipad_ground_truth()
 
-    if args.helipad_plots == "all":
-        plotter.plot_helipad_ground_truth()
-        plotter.plot_helipad_estimate()
-    elif args.helipad_plots == "est":
-        plotter.plot_helipad_estimate()
-    elif args.helipad_plots == "gt":
-        plotter.plot_helipad_ground_truth()
+    if args.estimate_plots == "all":
+        plotter.plot_drone_pose_dnn_cv()
+        plotter.plot_drone_pose_ekf()
+    elif args.estimate_plots == "dnn_cv_pose":
+        plotter.plot_drone_pose_dnn_cv()
+    elif args.estimate_plots == "ekf_pose":
+        plotter.plot_drone_pose_ekf()
 
     plt.show()
 
