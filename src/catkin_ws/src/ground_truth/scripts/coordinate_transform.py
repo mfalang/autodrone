55c571453bfe0f6ec197bfde26424d13bd12da83
Martin Falang <falang.martin@gmail.com>
2022-02-05 10:46:09 +0100

    Change to publish on receiving drone pose only
    Publishing the relative pose each time a new drone pose is received
    is much faster than waiting for both to be received at the same time.
    This approach is able to republish at >50 Hz which is good. The
    underlying assumption which has to hold for this to work is that the
    helipad moves very little, meaning that we can pick whichever pose
    we last received from it always. This should be plausible as the
    helipad will not perform any high speed maneuvers.
    
    The reported difference between the helipad messages are mostly less
    than one millisecond, with spikes at around 150 ms, but this should
    still be fine given that the helipad moves slowly.

--- src/catkin_ws/src/ground_truth/scripts/coordinate_transform.py
+++ src/catkin_ws/src/ground_truth/scripts/coordinate_transform.py
@@ -52,12 +52,17 @@ class CoordinateTransform():
 
 
     def _drone_gt_pose_cb(self, msg: geometry_msgs.msg.PoseStamped):
-        self.newest_drone_pose_msg = msg
-        self.new_drone_gt_pose = True
+        # Do the job here, and assume that the previously stored helipad position
+        # is correct as the helipad should not have any high speed maneuvers
+        if self.newest_helipad_pose_msg != None:
+            # print(f"Difference: {(msg.header.stamp - self.newest_helipad_pose_msg.header.stamp).to_sec()*1000}")
+            drone_pose_ned_frame = self._geometry_msg_pose_to_array(msg)
+            helipad_pose_ned_frame = self._geometry_msg_pose_to_array(self.newest_helipad_pose_msg)
+            drone_pose_helipad_frame = self._ned_to_helipad_frame(drone_pose_ned_frame, helipad_pose_ned_frame)
+            self._publish_drone_helipad_frame_pose(drone_pose_helipad_frame)
 
     def _helipad_gt_pose_cb(self, msg: geometry_msgs.msg.PoseStamped):
         self.newest_helipad_pose_msg = msg
-        self.new_helipad_gt_pose = True
 
     def _ned_to_helipad_frame(self, drone_pose_ned: np.ndarray, helipad_pose_ned: np.ndarray):
 
@@ -69,8 +74,6 @@ class CoordinateTransform():
             [0, 0, 1]
         ])
 
-        # print(R_ned_to_heli)
-
         # Translation between Helipad and NED frame (negative since this by definition
         # is the translation Helipad->NED expressed in the Helipad frame, but the
         # simulator and motion capture system gives the distance expressed in the
@@ -80,7 +83,6 @@ class CoordinateTransform():
             helipad_pose_ned[1],
             helipad_pose_ned[2]
         ])
-        # print(t_heli_to_ned_in_heli)
 
         # Convert position
         drone_position_ned = np.array([
@@ -89,7 +91,7 @@ class CoordinateTransform():
             drone_pose_ned[2]
         ])
         drone_position_helipad = R_ned_to_heli @ drone_position_ned + t_heli_to_ned_in_heli
-        print(drone_position_helipad)
+
         # Convert orientation
         # TODO
 
@@ -139,25 +141,7 @@ class CoordinateTransform():
     def start(self):
         rospy.loginfo("Starting ground truth coordinate frame alignment")
 
-        rate = rospy.Rate(100)
-
-        while not rospy.is_shutdown():
-            if not (self.new_drone_gt_pose and self.new_helipad_gt_pose):
-                continue
-
-            drone_pose_ned_frame = self._geometry_msg_pose_to_array(self.newest_drone_pose_msg)
-            helipad_pose_ned_frame = self._geometry_msg_pose_to_array(self.newest_helipad_pose_msg)
-            drone_pose_helipad_frame = self._ned_to_helipad_frame(drone_pose_ned_frame, helipad_pose_ned_frame)
-            self._publish_drone_helipad_frame_pose(drone_pose_helipad_frame)
-
-            # print(f"Difference: {(self.newest_drone_pose_msg.header.stamp - self.newest_helipad_pose_msg.header.stamp).to_sec()*1000}")
-
-
-
-            self.new_drone_gt_pose = False
-            self.new_helipad_gt_pose = False
-
-            rate.sleep()
+        rospy.spin()
 
 def main():
     coordinate_transformer = CoordinateTransform()
