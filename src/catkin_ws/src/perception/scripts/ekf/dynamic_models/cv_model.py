0ab9f58237836b667f0ed5e1cf4e6aacb2fb21ed
Martin Falang <falang.martin@gmail.com>
2022-02-25 12:51:15 +0100

    Add constant velocity dynamic model

---
+++ src/catkin_ws/src/perception/scripts/ekf/dynamic_models/cv_model.py
@@ -0,0 +1,76 @@
+
+import numpy as np
+
+class ConstantVelocityModel():
+
+    _Q = np.diag([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 5, 5, 5])
+    n = 9 # 3 pos, 3 vel, 3 attitude
+
+    # Attitude parameters
+    tau_phi = 0.1
+    k_phi = 0.5
+    tau_theta = 0.1
+    k_theta = 0.5
+
+    def f(self, x: np.ndarray, u: np.ndarray, dt: float):
+        """
+        Calculate zero-noise transition from current x
+
+        x = [helipad position in drone body frame (x,y,z),
+             helipad velocity in drone body frame (v_x, v_y, v_z),
+             drone attitude (phi, theta, psi)]
+
+        u = [roll reference (phi_ref),
+             pitch reference (theta_ref),
+             yaw rate reference (psi_dot_ref),
+             climb rate reference (z_dot_ref)]
+        """
+
+        x_next = np.zeros_like(x, dtype=float)
+
+        # Position prediction
+        x_next[0] = x[0] + dt*x[3]
+        x_next[1] = x[1] + dt*x[4]
+        x_next[2] = x[2] + dt*x[5]
+
+        # Velocity prediction
+        x_next[3] = x_next[3]
+        x_next[4] = x_next[4]
+        x_next[5] = x_next[5]
+
+        # Attitude prediction
+        x_next[6] = x[6] + dt * 1/self.tau_phi (self.k_phi*u[0] - x[6])
+        x_next[7] = x[7] + dt * 1/self.tau_theta (self.k_theta*u[1] - x[7])
+        x_next[8] = x[8] + dt * u[2]
+
+        return x_next.copy()
+
+    def F(self, x, u, dt):
+        """
+        Calculate transition function Jacobian
+        """
+
+        F_pos = np.array([
+            [1, 0, 0, dt, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, dt, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, dt, 0, 0, 0]
+        ])
+
+        F_vel = np.array([
+            [0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0]
+        ])
+
+        F_att = np.array([
+            [0, 0, 0, 0, 0, 0, 1 - dt/self.k_phi, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1- dt/self.k_theta, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1]
+        ])
+
+        F = np.vstack((F_pos, F_vel, F_att))
+
+        return F.copy()
+
+    def Q(self, x, dt):
+        return self._Q.copy()
